48d21dae03b0ff789b3d32c080eaf6bd0f55a0bc
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-03-28 22:17:59 -0700

    updated routers

--- src/router.js
+++ src/router.js
@@ -15,11 +15,13 @@ const router=createBrowserRouter(createRoutesFromElements(
     
             <Route path="expense/:id" element={<Expense/>} loader={ExpenseLoader} />
             <Route path="expenses/new" element={<NewExpense/>} />
+            <Route path="expenses" element={<ExpensesLoader />} />
             <Route path="create" action={createExpense} />
             <Route path="update/:id" action={updateExpense} />
             <Route path="delete/:id" action={deleteExpense} />
             <Route path="goal/:id" element={<Goal />} loader={GoalLoader} />
             <Route path="goals/new" element={<NewGoal/>} />
+            <Route path="goals" element={<GoalsLoader />} />
             <Route path="create" action={createGoal} />
             <Route path="update/:id" action={updateGoal} />
             <Route path="delete/:id" action={deleteGoal} />
